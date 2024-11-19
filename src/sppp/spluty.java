debc1cd4107ac56ea71d6b6612badf05de24d8c7
achal <achal@192.168.1.42>
2024-11-19 20:18:34 +0530

    yttc

---
+++ src/sppp/spluty.java
@@ -0,0 +1,10 @@
+package sppp;
+
+public class spluty {
+	public static void main(String args[]) {
+	System.out.println("**********************");
+	System.out.println("There is my home ");
+	System.out.println("*********************");
+	
+}
+}
